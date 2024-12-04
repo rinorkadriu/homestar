c7f83f81d5462b7a0874528eec8647b4e340b086
Rinor Kadriu <rinor.kadriu99@gmail.com>
2024-12-04 16:25:26 +0100

    Color variables and font added

--- src/app/views/home-page/home-page.component.ts
+++ src/app/views/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../../components/navigation/navigation.component';
 import { HomeBannerComponent } from "../../components/home-banner/home-banner.component";
+import { ProductsComponent } from '../../components/products/products.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavigationComponent, HomeBannerComponent],
+  imports: [NavigationComponent, HomeBannerComponent, ProductsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,17 @@ module.exports = {
         highBlack: '#000',
         highWhite: '#fff',
         highBrown: '#895737',
-        lightBrown: '#DAB49D'
+        lightBrown: '#DAB49D',
+        lowBlack: '#1b1b1b',
+        lowWhite: '#f1f1f1',
+        blackBacground: '#131312'
       },
       borderWidth: {
         '3': '3px',
       },
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif']
+        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif']
       }
     },
   },
