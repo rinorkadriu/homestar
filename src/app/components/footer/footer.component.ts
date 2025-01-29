4c7013a617a8f9fa2b912fcbb19eef9dedd697e9
Rinor Kadriu <rinor.kadriu99@gmail.com>
2025-01-29 23:06:59 +0100

    Footer animation on scroll

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  isVisible = false; // Track visibility of the footer
+
+  ngOnInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.isVisible = true; // Set to true when footer is in view
+          observer.unobserve(entry.target); // Stop observing once it's visible
+        }
+      });
+    });
+
+    const footerElement = document.querySelector('.footer');
+    if (footerElement) {
+      observer.observe(footerElement); // Observe the footer element
+    }
+  }
 
 }
